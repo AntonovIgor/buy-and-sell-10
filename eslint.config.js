74e43d34f1af89c755fdb3d4444607a7cd79048e
Igor Antonov <antonov.igor.khv@gmail.com>
2025-02-11 00:55:32 +0300

    Fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -38,6 +38,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-declaration-merging": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unnecessary-type-parameters": "off",
+      "@typescript-eslint/no-misused-promises": "off",
       "@typescript-eslint/no-misused-spread": "off"
     }
   },
